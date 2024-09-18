95fd39765197dbe4a5d8ed6221342da8ce51ea6a
Calle Trogen <calleat@hotmail.com>
2024-09-18 15:06:33 +0200

    footer style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,34 @@
 import React from "react"
+import styles from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <div className="">
+        <div className={styles.footerContainer}>
+            <div className="container">
+                <article>
+                    <p>Navigate</p>
+                    <li>Home</li>
+                    <li>Coffee</li>
+                    <li>Tea</li>
+                    <li>About Us</li>
+                </article>
+                <article>
+                    <p>Company</p>
+                    <li>About us</li>
+                    <li>Our products</li>
+                    <li>Partners</li>
+                </article>
+                <article>
+                    <p>Social media</p>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>LinkedIn</li>
+                </article>
+            </div>
+            <footer>
+                <p>JavaJunction &copy;</p>
+                <a href="*">javajunction@example.com</a>
+            </footer>
         </div>
     )
 }
