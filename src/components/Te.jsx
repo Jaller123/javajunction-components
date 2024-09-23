820efbf24fb9f5c0c18e2f82e6b247557758c3bc
Ahmed Idiris <aamedidris1999@gmail.com>
2024-09-23 11:14:37 +0200

    Update in Te.jsx and Te.module.css

--- src/components/Te.jsx
+++ src/components/Te.jsx
@@ -1,10 +1,18 @@
-import React from "react";
-const Te = () => {
-  return (
-    <>
-      <div>Te</div>
-    </>
-  );
-};
 
-export default Te;
+import React from "react";
+import styles from './Te.module.css'; 
+const Te = () => (
+  <div className={styles.container}>
+    <h1>Tea</h1>
+    <div className={styles.grid}>
+      {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+        <div className={styles.item} key={item}>
+          <img src={`https://via.placeholder.com/150?text=Image+${item}`} alt={`Image ${item}`} />
+          <button className={styles.button}>Add to cart</button>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
+export default Te; 
