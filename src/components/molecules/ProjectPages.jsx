0ac4cc4b1b493d5db9f52841621ea0a2d4f9eff1
Ahmed Idiris <aamedidris1999@gmail.com>
2024-09-23 15:09:11 +0200

    new update

--- src/components/molecules/ProjectPages.jsx
+++ src/components/molecules/ProjectPages.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import CoffeeImg from "../atoms/CoffeeImg"; // Justera sökväg om det behövs
-import AddToCartBtn from "../atoms/AddToCartBtn"; // Importera knappen
-import styles from "./ProjectPages.module.css"; // Skapa en CSS-modul om du behöver stilar
-
+import CoffeeImg from "../atoms/CoffeeImg"; 
+import AddToCartBtn from "../atoms/AddToCartBtn"; 
+import styles from "./ProjectPages.module.css";
 const ProjectPages = () => {
   return (
     <div className={styles.itemContainer}>
       <CoffeeImg />
-      <AddToCartBtn /> {/* Använd AddToCartBtn här */}
+      <AddToCartBtn /> 
     </div>
   );
 };
