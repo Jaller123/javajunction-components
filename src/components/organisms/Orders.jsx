d3772be69d7d26c87129c469da4c62e4feabbcc7
Kenath Liyanage <jallerken@hotmail.se>
2024-09-23 15:46:17 +0200

    Fixade CSS Orders

--- src/components/organisms/Orders.jsx
+++ src/components/organisms/Orders.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import styles from "./Orders.module.css"
+import OrdersContainer from "../molecules/OrdersContainer"
+import OrderSummary from "../molecules/OrderSummary"
+import ShoppingCart from "../molecules/ShoppingCart"
+import styles from './Orders.module.css';
+
+
+
 
 const Orders = () => {
   return (
-    <>
-    <div className={styles.ordersContainer}>
-    <div className={styles.shoppingCart} />
-    <div className={styles.orderSummary} />
+  <>
+<div className={styles.ShoppingCart}>
+  <div className={styles.OrderSummary}>
+    <div className={styles.ordersContainer}>  
+    <OrdersContainer />  
+    <OrderSummary /> 
+    <ShoppingCart />
     </div>
-    </>
+  </div>
+</div>
+  </>
   )
 }
 
