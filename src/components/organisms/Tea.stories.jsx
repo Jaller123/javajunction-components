862368f4b86349f6d2c9961bd01aa83d493f5a5e
Ahmed Idiris <aamedidris1999@gmail.com>
2024-09-23 11:46:07 +0200

    mina ändringar

--- src/components/organisms/Tea.stories.jsx
+++ src/components/organisms/Tea.stories.jsx
@@ -1,8 +1,8 @@
-import Te from "./Tea";
+import Tea from "./Tea"
 
 export default {
-  title: "javajunction-frontend/components/organisms/Te",
-  component: Te,
+  title: "javajunction-frontend/components/organisms/Tea",
+  component: Tea,
 };
 
 export const Default = {
