8220bc3ebea371ec645c0b5b4f03ea663bab17c0
Robin Bal <balrobin@hotmail.com>
2024-09-24 11:46:53 +0200

    Lagt in coffee organism

--- src/components/organisms/Coffee.jsx
+++ src/components/organisms/Coffee.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-
+import CoffeeImg from "../atoms/CoffeeImg";
+import AddToCartBtn from "../atoms/AddToCartBtn";
+import styles from "./Tea.module.css";
 const Coffee = () => {
+  const items = Array.from({ length: 8 }, (_, index) => index + 1);
+
   return (
-    <>
-      <div>Coffee</div>
-    </>
+    <div className={styles.pageContainer}>
+      <h1>Coffee</h1>
+
+      <div className={styles.gridContainer}>
+        <div className={styles.topSection}>
+          {items.slice(0, 4).map((item) => (
+            <div key={item} className={styles.item}>
+              <CoffeeImg />
+              <AddToCartBtn />
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.bottomSection}>
+          {items.slice(4, 8).map((item) => (
+            <div key={item} className={styles.item}>
+              <CoffeeImg />
+              <AddToCartBtn />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
