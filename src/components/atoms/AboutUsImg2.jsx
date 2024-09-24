f6ad27e5a56b4cca1bc97ff9640b978ffc105704
Tomas Yacoub <tomasyacoub@hotmail.com>
2024-09-24 14:03:49 +0200

    pics2

---
+++ src/components/atoms/AboutUsImg2.jsx
@@ -0,0 +1,11 @@
+import AboutUsImg2 from "../../Pictures/Kaffe-Bild3.jpg"
+import styles from "../atoms/AboutUsImg.module.css"
+
+const AboutUsImg2 = () => {
+    
+    return (
+        <img src={AboutUsImg2} alt="" className={styles.AboutUsImg2}/>
+    )
+}
+
+export default AboutUsImg2;
