518c20fd2f02a98eb1d234c08b5545b3be38d6d5
Tomas Yacoub <tomasyacoub@hotmail.com>
2024-09-24 13:38:37 +0200

    pics

---
+++ src/components/atoms/AboutUsImg.jsx
@@ -0,0 +1,11 @@
+import AboutUsImg from "../../Pictures/Kaffe-Bild2.jpg"
+import styles from "../atoms/AboutUsImg.module.css"
+
+const AboutUsImg = () => {
+    
+    return (
+        <img src={AboutUsImg} alt="" className={styles.AboutUsImg}/>
+    )
+}
+
+export default AboutUsImg;
---
+++ src/components/atoms/AboutUsImg.stories.jsx
@@ -0,0 +1,8 @@
+import AboutUsImg from "./AboutUsImg"
+
+export default {
+    title: "javajunction-frontend/components/atoms/AboutUsImg",
+    component: AboutUsImg,
+};
+
+export const Default = {};
