d09231f4e793f97d1f58c3907a9957f9c1b88e02
Tomas Yacoub <tomasyacoub@hotmail.com>
2024-10-08 14:20:02 +0200

    titeltext

---
+++ src/components/atoms/AboutUsText.stories.jsx
@@ -0,0 +1,8 @@
+import AboutUsText from "./AboutUsText";
+
+export default {
+    title: "javajunction-frontend/components/atoms/AboutUsText",
+    component: AboutUsText,
+};
+
+export const Default = {};
