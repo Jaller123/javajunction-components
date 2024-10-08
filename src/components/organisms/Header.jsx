94e887322bc39a03207ec9df4cdb2ec1cbdf6809
MohamedElmi10 <144214489+MohamedElmi10@users.noreply.github.com>
2024-10-08 11:44:38 +0200

    Add support for custom className in Navlink component for the second time

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Logo from "../atoms/Logo";
 import styles from "./Header.module.css";
 import NavLinks from "../molecules/NavLinks";
-import CartIcon from "../atoms/CartIcon/CartIcon"
-const Header = () => {
+
+const Header = ({ navLinksClassName }) => {
   return (
     <>
       <div className={styles.header}>
         <Logo />
-        <NavLinks />
-        {/* // <CartIcon /> */}
+        <NavLinks className={navLinksClassName} />
+
       </div>
     </>
   );
